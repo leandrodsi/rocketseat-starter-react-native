343b25dd478d4a972fc9403914327e57fdb8b36a
Leandro Domingos <leandrodsystems@gmail.com>
2020-02-04 21:26:24 -0300

    Aplicacao construída e funcional

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:3001/api',
+// });
+
+const api = axios.create({
+  baseURL: 'https://rocketseat-node.herokuapp.com/api',
+});
+
+export default api;
